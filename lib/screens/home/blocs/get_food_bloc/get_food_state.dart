4a70c1a4f5519731fe565bbfe033421f01ddbc21
MoshDevApp <motaz33345@gmail.com>
2025-12-12 02:17:16 +0800

    spot 4 Done

--- lib/screens/home/blocs/get_food_bloc/get_food_state.dart
+++ lib/screens/home/blocs/get_food_bloc/get_food_state.dart
@@ -1,6 +1,21 @@
-// part of 'get_food_bloc.dart';
+part of 'get_food_bloc.dart';
 
-// @immutable
-// sealed class GetFoodState {}
+sealed class GetFoodState extends Equatable {
+  const GetFoodState();
+  
+  @override
+  List<Object> get props => [];
+}
 
-// final class GetFoodInitial extends GetFoodState {}
+final class GetFoodInitial extends GetFoodState {}
+
+final class GetFoodFailure extends GetFoodState {}
+final class GetFoodLoading extends GetFoodState {}
+final class GetFoodaSuccess extends GetFoodState {
+  final List<Food> foods;
+
+  const GetFoodSuccess(this.foods);
+
+  @override
+  List<Object> get props => [foods];
+}
