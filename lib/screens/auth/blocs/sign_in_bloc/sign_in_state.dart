2578e99546eb766c56176f2b8ebfa654479cf81e
Thiha-14 <phyo14thiha@gmail.com>
2025-12-17 14:36:19 +0800

    thiha

--- lib/screens/auth/blocs/sign_in_bloc/sign_in_state.dart
+++ lib/screens/auth/blocs/sign_in_bloc/sign_in_state.dart
@@ -4,3 +4,18 @@
 // sealed class SignInState {}
 
 // final class SignInInitial extends SignInState {}
+
+part of 'sign_in_bloc.dart';
+
+sealed class SignInState extends Equatable {
+  const SignInState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+final class SignInInitial extends SignInState {}
+
+final class SignInFailure extends SignInState {}
+final class SignInProcess extends SignInState {}
+final class SignInSuccess extends SignInState {}
--- lib/screens/auth/blocs/sign_up_bloc/sign_up_state.dart
+++ lib/screens/auth/blocs/sign_up_bloc/sign_up_state.dart
@@ -4,3 +4,18 @@
 // sealed class SignUpState {}
 
 // final class SignUpInitial extends SignUpState {}
+
+part of 'sign_up_bloc.dart';
+
+sealed class SignUpState extends Equatable {
+  const SignUpState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+final class SignUpInitial extends SignUpState {}
+
+class SignUpSuccess extends SignUpState {}
+class SignUpFailure extends SignUpState {}
+class SignUpProcess extends SignUpState {}
